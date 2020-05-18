a521622d5a09df707778fd9f2d7d17f871d96ba9
jiangyilong <jiangyilong@wafersystems.com>
2020-05-18 16:49:02 +0800

    add build

---
+++ build/precache-manifest.4de7d5df3be50a5c0a785fe9daa7d0c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8552cc272a2343f2b2512bb9438569ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fdc416e6442a1645b1b",
+    "url": "/static/js/2.104d408b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.104d408b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a17a00d88d5f620c4b",
+    "url": "/static/js/main.c7df2522.chunk.js"
+  },
+  {
+    "revision": "bd1f84241319a29d7fde",
+    "url": "/static/js/runtime-main.4d873de0.js"
+  }
+]);
